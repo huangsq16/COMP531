84d598145443e897dd8668420af46cc419f96842
HaoyuanY <yuehaoyuan@gmail.com>
2017-03-01 18:57:31 -0600

    Add files via upload

---
+++ inclass-14/e2e/common.js
@@ -0,0 +1,25 @@
+import { expect } from 'chai'
+import { findId, sleep } from './selenium'
+
+// TODO add your test user credentials here!
+exports.creds = {
+    username: 'sep1test',
+    password: 'native-web-tester'
+}
+
+exports.login = () =>
+    sleep(500)
+        .then(findId('username').clear())
+        .then(findId('password').clear())
+        .then(findId('username').sendKeys(exports.creds.username))
+        .then(findId('password').sendKeys(exports.creds.password))
+        .then(findId('login').click())
+        .then(sleep(2000))
+
+exports.logout = () =>
+    sleep(500)
+        .then(findId('logout').click())
+        .then(findId('message').getText())
+        .then((t) => {expect(t).to.equal('You have logged out')})
+    // IMPLEMENT ME
+    // validate the message says: 'You have logged out'
